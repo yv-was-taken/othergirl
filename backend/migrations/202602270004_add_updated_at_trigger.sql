4dfaf2f0fa2b8a1edd0ffae1577c123a158a1fc6
yv-was-taken <yvmail@proton.me>
2026-02-26 21:00:10 -0600

    Add updated_at trigger for users table

---
+++ backend/migrations/202602270004_add_updated_at_trigger.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION set_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_users_updated_at
+    BEFORE UPDATE ON users
+    FOR EACH ROW
+    EXECUTE FUNCTION set_updated_at();
